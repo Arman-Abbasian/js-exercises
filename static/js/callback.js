ff165d5e80cabfcb686489e486f4329540431b0e
Arman Abbasian <abasian.arman@gmail.com>
2022-07-14 10:05:27 +0430

    callback promise async

--- static/js/callback.js
+++ static/js/callback.js
@@ -1,6 +1,6 @@
 const person=(id,callback)=>{
 setTimeout(()=>{
-    callback({name:"Ali",weight:75,height:180})
+    callback({name:"Ali",weight:55,height:180})
 },3000)
 };
 const bmiCalculation=(weight,height,callback)=>{
@@ -9,11 +9,20 @@ const bmiCalculation=(weight,height,callback)=>{
  },2000)
 };
 const informConclusion=(bmi,callback)=>{
+    let condition="";
     setTimeout(()=>{
-        callback(`your bmi is ${bmi}`)
+        if(bmi<20) condition="less"
+        if(bmi>=20 && bmi<25) condition="normal"
+        if(bmi>=25) condition="over"
+        callback(`your bmi is ${bmi} and is ${condition}`)
     },2000)
-}
+};
 
-person(1,(data)=>{
-    console.log(data);
-})
+const conslusion=person(1,(data)=>{
+    bmiCalculation(data.weight,data.height,(bmi)=>{
+        informConclusion(bmi,(conc)=>{
+           console.log(conc)
+        });
+    })
+});
+
--- static/js/promise.js
+++ static/js/promise.js
@@ -1,42 +1,105 @@
-function data(id){
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            const condition=true;
-            if(condition){
-                resolve({name:"Ali",height:180,weight:75})
-            }else{
-                reject(new Error("your id is not in the database"))
-            }
-        },1000)
-    })
-};
+// function data(id){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             const condition=true;
+//             if(condition){
+//                 resolve({name:"Ali",height:180,weight:75})
+//             }else{
+//                 reject(new Error("your id is not in the database"))
+//             }
+//         },1000)
+//     })
+// };
 
-function calc(weight,height){
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            const condition=true;
-            if(condition){
-                resolve(Math.abs(weight / Math.pow((height / 100),2)))
-            }else{
-                reject(new Error("some problec occured"))
-            }
-        },1000)
-    })
-};
 
-function bm(calc){
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            const condition=true;
-            if(condition){
-                resolve("your BMI is "+calc)
+
+// function calc(weight,height){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             const condition=true;
+//             if(condition){
+//                 resolve(Math.abs(weight / Math.pow((height / 100),2)))
+//             }else{
+//                 reject(new Error("some problec occured"))
+//             }
+//         },1000)
+//     })
+// };
+
+// function bm(calc){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             const condition=true;
+//             if(condition){
+//                 resolve("your BMI is "+calc)
+//             }else{
+//                 reject(new Error("some problec occured"))
+//             }
+//         },1000)
+//     })
+// };
+// data(45).then((user)=>{
+//     calc(user.weight,user.height).then((bmi)=>{bm(bmi)
+//         .then((bmm)=>console.log(bmm))})
+// })
+
+
+const person=(id)=>{
+   return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        if(server="open"){
+        resolve({name:"Ali",weight:55,height:180})
+    }else{
+        reject(new Error("some problem occured"))
+    }
+    },1000);
+   })
+    };
+
+    const bmiCalculation=(weight,height)=>{
+        return new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                if(server="open"){
+                resolve(Math.round(weight / Math.pow((height / 100),2)));
             }else{
-                reject(new Error("some problec occured"))
+                reject(new Error("some problem occured"));
             }
-        },1000)
-    })
-};
-data(45).then((user)=>{
-    calc(user.weight,user.height).then((bmi)=>{bm(bmi)
-        .then((bmm)=>console.log(bmm))})
-})
+             },1000)
+        })
+    
+    };
+
+    const informConclusion=(bmi)=>{
+        return new Promise((resolve,reject)=>{
+            let condition="";
+            setTimeout(()=>{
+                if(bmi<20) condition="less"
+                if(bmi>=20 && bmi<25) condition="normal"
+                if(bmi>=25) condition="over"
+                if(server="open"){
+                resolve(`your bmi is ${bmi} and is ${condition}`)
+            }else{
+                reject(new Error("some problem occured"));
+            }
+            },1000)
+            })
+    };
+
+//   person(10)
+//   .then(data=>bmiCalculation(data.weight,data.height))
+//   .then(bmi=>informConclusion(bmi))
+//   .then((bmi)=>console.log(bmi))
+//   .catch(err=>console.log(err.message));
+
+const bmi=async()=>{
+    try {
+        const personn=await person(58);
+        const bmiCalculationn=await bmiCalculation(personn.weight,personn.height)
+        const informConclusionn=await informConclusion(bmiCalculationn);
+        console.log(informConclusionn)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+bmi()
+
